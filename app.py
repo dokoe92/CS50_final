3be1f89bab528883f00be348bec1efdc19a896ee
Dominik Köberl <dokoe92@gmail.com>
2022-08-27 21:16:45 +0200

    added simple bootstrap navigation bar and basic jinja templates for layout and start

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("start.html")
+
