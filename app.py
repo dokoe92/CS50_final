54c121468acf404084aab1df56538f66184a726f
Dominik Köberl <dokoe92@gmail.com>
2022-08-28 19:21:02 +0200

    added login section and the backend connection

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ from helpers import failure
 app=Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
-
+app.config["SECRET_KEY"] = "hallosecretkey"
 
 
 @app.route("/", methods=["GET"])
+
+
+@app.route("/welcome")
 def index():
-    return render_template("start.html")
+    return render_template("welcome.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -42,8 +46,54 @@ def register():
             return failure("Username already exists!")
         return redirect("/")
         
-
-
     else:
         return render_template("register.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        con = sqlite3.connect("timoro.db")
+        con.row_factory = sqlite3.Row # Convert tuples from SQL query to a list of dict
+        c = con.cursor()
+
+        if not request.form.get("username"):
+            return failure("Please insert username!")
+
+        elif not request.form.get("password"):
+            return failure("Please insert password!")
+        
+        # Check if user is in database
+        username = request.form.get("username")
+        password = request.form.get("password")
+        error = None
+        user = c.execute("SELECT * FROM users WHERE username = ?", (username,))
+        user = c.fetchone()
+        
+        if user is None:
+            error = "Incorrect username."
+            return failure("Incorrect username or password")
+        elif not check_password_hash(user["hash"], password):
+            error = "Incorrect password."
+            return failure("Incorrect password or password")
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect("/")
+        
+    else:
+        return render_template("login.html")
+        
+
+
+
+    
+    
+
+
+
+
+
+
 
