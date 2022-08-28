b0bac1f8a0438520800d40698b3402452f0349c3
Dominik Köberl <dokoe92@gmail.com>
2022-08-28 09:49:14 +0200

    added register form and buttons for it

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, url_for
+import sqlite3
 
 app=Flask(__name__)
 
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Add 
 
 
-@app.route("/")
+
+@app.route("/", methods=["GET"])
 def index():
     return render_template("start.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("password_confirm")
+
+    else:
+        return render_template("register.html")
 
