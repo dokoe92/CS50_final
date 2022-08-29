6160baa7112871dfb09a85930f7076d6b6a522fa
Dominik Köberl <dokoe92@gmail.com>
2022-08-29 21:58:25 +0200

    added pomodoro.js and added pause + reset button, set up variables for pomodoro buttons in js

---
+++ static/pomodoro.js
@@ -0,0 +1,18 @@
+var start = document.getElementById("start");
+
+var set_pomodoro = document.getElementById("set_pomodoro");
+var set_short_break = document.getElementById("set_short_break");
+var set_long_break = document.getElementById("set_long_break");
+
+var minutes = document.getElementById("minutes");
+var seconds = document.getElementById("seconds");
+
+
+
+
+
+
+
+start.addEventListener("click", function(){
+    
+})
